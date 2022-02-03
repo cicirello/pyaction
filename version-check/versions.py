af8b7a54dc2aa7b5c4f817c788b17cf45445a68a
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-02-03 12:15:52 -0500

    Automated changelog update

--- version-check/versions.py
+++ version-check/versions.py
@@ -28,6 +28,7 @@
 import json
 import subprocess
 import platform
+from datetime import datetime
 
 def executeCommand(arguments) :
     """Execute a subprocess and return result and exit code.
@@ -74,6 +75,96 @@ def parse_gpg_version(gpg) :
     """
     return gpg.split()[2]
 
+def updateChangelogIfNecessary(versions) :
+    """Compares current versions to old, and if any changed
+    updates the Changelog. Returns whether or not any versions
+    have changed.
+
+    Keyword arguments:
+    versions - Python map containing the versions extracted from
+        the Docker image.
+    """
+    with open("versions.json", "r") as f :
+        priorVersions = json.load(f)
+    changed = versions != priorVersions
+    if changed :
+        updateChangelog(priorVersions, versions)
+    return changed
+
+def updateVersionsJSON(versions) :
+    """Updates versions.json as necessary.
+
+    Keyword arguments:
+    versions - Python map containing the versions extracted from
+        the Docker image.
+    """
+    with open("versions.json", "w") as f :
+        json.dump(versions, f, sort_keys=True, indent=2)
+
+def updateChangelog(priorVersions, versions) :
+    """Updates the Unreleased section of the Changelog as necessary.
+
+    Keyword arguments:
+    priorVersions - Python map containing the versions as listed
+        currently in the existing versions.json.
+    versions - Python map containing the versions extracted from
+        the Docker image.
+    """
+    with open("CHANGELOG.md", "r+") as f :
+        contents = f.readlines()
+        unreleasedLineNumber = None
+        changedStartsAt = None
+        changedEndsAt = None
+        for i, line in enumerate(contents) :
+            if unreleasedLineNumber == None and line.strip().startswith("## [Unreleased]") :
+                unreleasedLineNumber = i
+            elif changedStartsAt != None and line.strip().startswith("###") :
+                changedEndsAt = i - 1
+            elif unreleasedLineNumber != None and line.strip().startswith("### Changed") :
+                changedStartsAt = i + 1
+        contents[unreleasedLineNumber] = "## [Unreleased] - " + datetime.today().strftime('%Y-%m-%d')
+        changedContents = findChanges(priorVersions, versions)
+        updatedLogLines = set()
+        for i in range(changedStartsAt, changedEndsAt) :
+            if contents[i].startswith("* Bumped") :
+                j = contents[i].find(" to ")
+                if j > 0 :
+                    checkThis = contents[i][:j+3]
+                    for k, toolLine in enumerate(changedContents) :
+                        if toolLine.startswith(checkThis) :
+                            contents[i] = toolLine
+                            updatedLogLines.add(k)
+                            break
+        changedContents = [ toolLine for k, toolLine in enumerate(changedContents) if k not in updatedLogLines]
+        if len(changedContents) > 0 :
+            contents[changedStartsAt:changedStartsAt] = changedContents
+        f.seek(0)
+        f.truncate()
+        f.writelines(contents)
+
+def findChanges(priorVersions, versions) :
+    """Determines which tools have new versions.
+
+    Keyword arguments:
+    priorVersions - Python map containing the versions as listed
+        currently in the existing versions.json.
+    versions - Python map containing the versions extracted from
+        the Docker image.
+    """
+    template = "* Bumped {0} to {1}.\n"
+    changes = []
+    if priorVersions["python"] != versions["python"] :
+        changes.append(template.format("Python", versions["python"]))
+    if priorVersions["gh"] != versions["gh"] :
+        changes.append(template.format("GitHub CLI", versions["gh"]))
+    if priorVersions["git"] != versions["git"] :
+        changes.append(template.format("git", versions["git"]))
+    if priorVersions["curl"] != versions["curl"] :
+        changes.append(template.format("curl", versions["curl"]))
+    if priorVersions["gpg"] != versions["gpg"] :
+        changes.append(template.format("gpg", versions["gpg"]))
+    return changes
+
 if __name__ == "__main__" :
     gh, code = executeCommand(["gh", "--version"])
     gh = parse_gh_version(gh)
@@ -95,5 +186,5 @@ if __name__ == "__main__" :
         "gpg" : gpg,
         "python" : platform.python_version()
     }
-    with open("versions.json", "w") as f :
-        json.dump(versions, f, sort_keys=True, indent=2)
+    if updateChangelogIfNecessary(versions) :
+        updateVersionsJSON(versions)
